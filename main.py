b03cc9d7a02cdf22f1fbb2ac2c174e5278de8b73
Mew <54446552+Mayamee@users.noreply.github.com>
2022-05-16 20:51:39 +0300

    Frontend

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from distutils.log import debug
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def hello():
     return render_template("index.html")
@@ -11,14 +13,14 @@ def my_form_post():
     theta1 = request.form['input_text1']
     theta2 = request.form['input_text2']
     theta3 = request.form['input_text3']
-    printer(theta1, theta2, theta3)
+    printer(int(float(theta1)), theta2, theta3)
     return theta1
 
 def printer(theta1, theta2, theta3):
     print([theta1, theta2, theta3])
 
 if __name__ == "__main__":
-    # app.run(debug='true')
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug='true')
+    #app.run(host='0.0.0.0', port=5000)
 
 
---
+++ static/js/main.js
@@ -0,0 +1,72 @@
+const t1 = document.getElementById("rangeinput1");
+const t2 = document.getElementById("rangeinput2");
+const t3 = document.getElementById("rangeinput3");
+
+const text1 = document.getElementById("input_text1");
+const text2 = document.getElementById("input_text2");
+const text3 = document.getElementById("input_text3");
+
+function sync() {
+  const { value } = this;
+
+  this.value = this.value
+    .replace("-0", "0")
+    .replace(/\D/g, (sign) => (sign === "-" ? sign : ""))
+    .replace(/-{1,}/, "-")
+    .replace(/(?<=\d)-+$/, "");
+
+  if (value.length > 0 && value <= 100 && value >= -100) {
+    this.nextElementSibling.value = this.value;
+    sendValue();
+  }
+  if (value.length > 0 && value <= -100) {
+    this.value = -100;
+    this.nextElementSibling.value = this.value;
+  }
+
+  if (value.length > 0 && value >= 100) {
+    this.value = 100;
+    this.nextElementSibling.value = this.value;
+  }
+}
+
+text1.addEventListener("input", sync);
+text2.addEventListener("change", sync);
+text3.addEventListener("change", sync);
+
+t1.addEventListener("input", function () {
+  this.previousElementSibling.value = this.value;
+  sendValue();
+});
+
+t2.addEventListener("input", function () {
+  this.previousElementSibling.value = this.value;
+  sendValue();
+});
+
+t3.addEventListener("input", function () {
+  this.previousElementSibling.value = this.value;
+  sendValue();
+});
+
+function sendValue() {
+  $.ajax({
+    type: "POST",
+    url: "/get_value",
+    data: $("form").serialize(),
+    cache: false,
+    async: true,
+    enctype: "mutipart/form-data",
+    type: "POST",
+    success: function (data) {
+      var json = jQuery.parseJSON(data);
+      //alert('success');
+      $("#len").html(json.len);
+      // console.log(data);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+    timeout: 0,
+  });
+}
